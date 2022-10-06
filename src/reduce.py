b547979a8b5b393f5981e583a1bc4295bf37000f
Ingeborg3 <111577212+Ingeborg3@users.noreply.github.com>
2022-10-06 20:38:48 +0200

    second

--- src/reduce.py
+++ src/reduce.py
@@ -5,18 +5,6 @@ A = TypeVar('A')
 B = TypeVar('B')
 # Hvad gør TypeVar og Callable?
 
-#def sum(x: int, y: int) -> int:
-#    """
-#    Compute sum of two integers.
-#    """
-#    return x+y
-
-#def product(x: int, y: int) -> int:
-#    """ 
-#    Compute product of two integers.
-#    """
-#    return x*y
-
 def reduce(f: Callable[[A], B], x: list[A]) -> B:
     """
     Reduce f over list x.x
@@ -26,16 +14,20 @@ def reduce(f: Callable[[A], B], x: list[A]) -> B:
     """
     assert len(x) >= 2
     
-    n = len(x)
-    y = [0]*n
-    y[0] = x[0]
+    # Version where the intermediate results are not stored in memory.
+    number = x[0]
     for i in range(1, len(x)):
-        y[i] = f(y[i-1], x[i])
-    return y[n-1]
+        number = f(number, x[i])
+    return number
 
-# Sum function is not to be used. Just call reduce() as below.
-# x reduced with f.
-# Hvorfor reduce med kursiv?
+    # Version where memory to store intermediate results are used.
+    # n = len(x)
+    # y = [0]*n
+    # y[0] = x[0]
+    # for i in range(1, len(x)):
+    #    y[i] = f(y[i-1], x[i])
+    # return y[n-1]
+
 print(reduce(lambda x,y: x+y, [1, 2, 3]))
 print(reduce(lambda x,y: x*y, [1,2,3]))
 
